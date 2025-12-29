29067a8ca029a1e11ff3e9705e003b0c19c6cdd8
Louis Chatain <louischatain123@gmail.com>
2025-12-29 19:24:34 +0100

    added get_current_user function

---
+++ auth/oauth2.py
@@ -0,0 +1,47 @@
+# user_service/auth_deps.py
+import os
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from db import db_user
+from db.database import get_async_db
+
+# 1. Configuration
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+# 2. Define the scheme
+# This tells Swagger UI where to find the token (the URL of your auth service)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="http://localhost:8004/login")
+
+# 3. The Dependency
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_async_db)):
+    """
+    Decodes the token, verifies validity, and returns the User.
+    """
+    
+    try:
+        if not SECRET_KEY:
+            raise ValueError("CRITICAL: SECRET_KEY environment variable is required.")
+        if not ALGORITHM:
+            raise ValueError("CRITICAL: ALGORITHM environment variable is required.")
+        
+        # Decode the JWT
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        
+        # Extract the user ID
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Did not find the user id in the payload",
+            )
+    except JWTError as e:
+        raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f"Could not validate credentials: {e}",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    user = await db_user.read_user_by_user_id(int(user_id), db)
+    return user
