d9e03e5114217cdca03535e7e84386d333becc3c
Louis Chatain <louischatain123@gmail.com>
2026-01-18 19:43:47 +0100

    changed url to auth because of nginx

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -11,34 +11,38 @@ ALGORITHM = os.getenv("ALGORITHM")
 
 # 2. Define the scheme
 # This tells Swagger UI where to find the token (the URL of your auth service)
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="http://localhost:8004/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="http://localhost/auth/login")
 
 # 3. The Dependency
-async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_async_db)):
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_async_db)) -> int:
     """
     Decodes the token, verifies validity, and returns the User.
     """
-    
-    try:
-        if not SECRET_KEY:
-            raise ValueError("CRITICAL: SECRET_KEY environment variable is required.")
-        if not ALGORITHM:
-            raise ValueError("CRITICAL: ALGORITHM environment variable is required.")
-        
+    if not SECRET_KEY: 
+        raise ValueError("CRITICAL: SECRET_KEY environment variable is required.")
+    if not ALGORITHM:
+        raise ValueError("CRITICAL: ALGORITHM environment variable is required.")
+    try:    
         # Decode the JWT
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        
-        # Extract the user ID
-        user_id = payload.get("sub")
-        if user_id is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Did not find the user id in the payload",
-            )
     except JWTError as e:
         raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail=f"Could not validate credentials: {e}",
         headers={"WWW-Authenticate": "Bearer"},
-    )
-    return int(user_id)
+    )    
+    # Extract the user ID
+    user_id = payload.get("sub")
+    if user_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Did not find the user id in the payload",
+        )
+    
+    try:
+        return int(user_id)
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User ID format is invalid"
+        )
